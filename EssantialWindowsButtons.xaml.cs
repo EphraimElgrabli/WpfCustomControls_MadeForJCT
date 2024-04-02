c0d4f7a6492fccb9d194b74d45b1ee53963f8dc2
Ephraim Elgrabli <32434942+EphraimElgrabli@users.noreply.github.com>
2024-04-02 20:45:31 +0300

    Add files via upload

---
+++ EssantialWindowsButtons.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL.Theme
+{
+    /// <summary>
+    /// Interaction logic for EssantialWindowsButtons.xaml
+    /// </summary>
+    public partial class EssentialWindowsButtons : UserControl
+    {
+        public EssentialWindowsButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+    }
+}
